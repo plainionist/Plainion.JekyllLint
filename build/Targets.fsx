db67941a7aaec9c3f4b095220adf28558d14fbfb
plainionist <stanley751@gmx.net>
2021-09-03 07:42:21 +0200

    Targets.fsx migrated to latest Plainion.CI

--- build/Targets.fsx
+++ build/Targets.fsx
@@ -1,12 +1,15 @@
 // load dependencies from source folder to allow bootstrapping
-#r "/bin/Plainion.CI/FAKE/FakeLib.dll"
-#load "/bin/Plainion.CI/bits/PlainionCI.fsx"
+#r "/bin/Plainion.CI/Fake.Core.Target.dll"
+#r "/bin/Plainion.CI/Fake.Core.Process.dll"
+#r "/bin/Plainion.CI/Fake.IO.FileSystem.dll"
+#r "/bin/Plainion.CI/Fake.IO.Zip.dll"
+#r "/bin/Plainion.CI/Plainion.CI.Tasks.dll"
 
 open Fake.Core
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
-open PlainionCI
+open Plainion.CI
 
 Target.create "CreatePackage" (fun _ ->
     !! ( outputPath </> "*.*Specs.*" )
