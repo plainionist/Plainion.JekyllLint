0fc0d5410028c6fae7191f8a334fe229a58cb049
plainionist <stanley751@gmx.net>
2021-09-03 07:36:05 +0200

    Package created

--- src/Plainion.JekyllLint/AssemblyInfo.fs
+++ src/Plainion.JekyllLint/AssemblyInfo.fs
@@ -5,15 +5,15 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Plainion.JekyllLint")>]
 [<assembly: AssemblyProductAttribute("Plainion.JekyllLint")>]
 [<assembly: AssemblyDescriptionAttribute("Plainion.JekyllLint")>]
-[<assembly: AssemblyCopyrightAttribute("Copyright @ 2019")>]
-[<assembly: AssemblyVersionAttribute("2.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.0")>]
+[<assembly: AssemblyCopyrightAttribute("Copyright @ 2021")>]
+[<assembly: AssemblyVersionAttribute("2.1")>]
+[<assembly: AssemblyFileVersionAttribute("2.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Plainion.JekyllLint"
     let [<Literal>] AssemblyProduct = "Plainion.JekyllLint"
     let [<Literal>] AssemblyDescription = "Plainion.JekyllLint"
-    let [<Literal>] AssemblyCopyright = "Copyright @ 2019"
-    let [<Literal>] AssemblyVersion = "2.0"
-    let [<Literal>] AssemblyFileVersion = "2.0"
+    let [<Literal>] AssemblyCopyright = "Copyright @ 2021"
+    let [<Literal>] AssemblyVersion = "2.1"
+    let [<Literal>] AssemblyFileVersion = "2.1"
